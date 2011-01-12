b6739ed5c167372a1f3c861bd8d8ed0c80603ee8
stephan.eberle <stephan.eberle@9c7074dd-d9a8-471e-8b89-2a72648c76b9>
2011-01-12 18:30:26 +0000

    ASSIGNED - bug 116: Extend Eclipse Refresh action in order to force model reload https://bugzilla01.geensys.com/show_bug.cgi?id=116

--- experimental/aal/plugins/org.artop.ecuc.codegen.xpand.ui/src/org/artop/ecuc/codegen/xpand/ui/actions/LaunchEcucCodeGenAction.java
+++ experimental/aal/plugins/org.artop.ecuc.codegen.xpand.ui/src/org/artop/ecuc/codegen/xpand/ui/actions/LaunchEcucCodeGenAction.java
@@ -6,11 +6,22 @@ import gautosar.gecucparameterdef.GModuleDef;
 import org.artop.ecl.emf.model.IModelDescriptor;
 import org.artop.ecl.emf.model.ModelDescriptorRegistry;
 import org.artop.ecl.emf.util.EcorePlatformUtil;
+import org.artop.ecl.emf.workspace.loading.ModelLoadManager;
 import org.artop.ecl.platform.ui.util.ExtendedPlatformUI;
 import org.artop.ecuc.codegen.xpand.ui.internal.messages.Messages;
 import org.artop.ecuc.gautosar.xtend.typesystem.EcucMetaModel;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.transaction.NotificationFilter;
+import org.eclipse.emf.transaction.ResourceSetChangeEvent;
+import org.eclipse.emf.transaction.ResourceSetListener;
+import org.eclipse.emf.transaction.ResourceSetListenerImpl;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.emf.transaction.util.TransactionUtil;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.window.Window;
 import org.eclipse.jface.wizard.WizardDialog;
@@ -22,8 +33,10 @@ public class LaunchEcucCodeGenAction extends AbstractM2TAction {
 
 	public static final String DEFAULT_ROOT_DEFINE_NAME = "main"; //$NON-NLS-1$
 
-	protected IFile moduleConfigurationFile;
-	protected IFile moduleDefFile;
+	protected GModuleConfiguration moduleConfiguration;
+	protected GModuleDef moduleDef;
+
+	private ResourceSetListener resourceChangedListener;
 
 	public LaunchEcucCodeGenAction() {
 		super(Messages.menuItem_launchEcucCodeGen);
@@ -38,19 +51,75 @@ public class LaunchEcucCodeGenAction extends AbstractM2TAction {
 		if (selection.size() == 1) {
 			Object selected = selection.getFirstElement();
 			if (selected instanceof GModuleConfiguration) {
-				GModuleConfiguration moduleConfiguration = (GModuleConfiguration) selected;
-				moduleConfigurationFile = EcorePlatformUtil.getFile(moduleConfiguration);
-				GModuleDef moduleDefinition = moduleConfiguration.gGetDefinition();
-				moduleDefFile = EcorePlatformUtil.getFile(moduleDefinition);
-				return moduleConfigurationFile != null && moduleDefFile != null;
+				// Retrieve module configuration behind selection as well as module definition which it references
+				moduleConfiguration = (GModuleConfiguration) selected;
+				moduleDef = moduleConfiguration.gGetDefinition();
+
+				// Referenced module definition not available? (this can be the case when the AUTOSAR model behind the
+				// selected element has not been fully loaded yet or the referenced module definition is really
+				// missing)
+				if (moduleDef.eIsProxy()) {
+					// Trigger asynchronous loading of underlying AUTOSAR model including referenced AUTOSAR models in
+					// BSW Platform projects (i.e., AUTOSAR projects with Xpand/Xtend nature) to make sure that
+					// referenced module definition has a chance to get resolved
+					IFile moduleConfigurationFile = EcorePlatformUtil.getFile(moduleConfiguration);
+					IModelDescriptor moduleConfigurationModel = ModelDescriptorRegistry.INSTANCE.getModel(moduleConfigurationFile);
+					ModelLoadManager.INSTANCE.loadModel(moduleConfigurationModel, true, true, null);
+
+					// Install resource changed listener which updates this action's selection state once the loading
+					// has been finished
+					TransactionalEditingDomain editingDomain = TransactionUtil.getEditingDomain(moduleConfiguration);
+					if (editingDomain != null) {
+						if (resourceChangedListener == null) {
+							resourceChangedListener = createResourceChangedListener();
+						}
+						editingDomain.addResourceSetListener(resourceChangedListener);
+					}
+				} else {
+					// Uninstall resource changed listener in case there is any
+					TransactionalEditingDomain editingDomain = TransactionUtil.getEditingDomain(moduleConfiguration);
+					if (editingDomain != null) {
+						if (resourceChangedListener != null) {
+							editingDomain.removeResourceSetListener(resourceChangedListener);
+						}
+					}
+				}
+
+				return moduleDef != null && !moduleDef.eIsProxy();
 			}
 		}
 		return false;
 	}
 
+	/**
+	 * Creates a ResourceSetListener that detects (re-)loaded resources and updates this action's selection.
+	 */
+	protected ResourceSetListener createResourceChangedListener() {
+		return new ResourceSetListenerImpl(NotificationFilter
+				.createFeatureFilter(EcorePackage.eINSTANCE.getEResource(), Resource.RESOURCE__IS_LOADED).or(
+						NotificationFilter.createFeatureFilter(EcorePackage.eINSTANCE.getEResourceSet(), ResourceSet.RESOURCE_SET__RESOURCES))) {
+			@Override
+			public void resourceSetChanged(ResourceSetChangeEvent event) {
+				// Update this action's selection state
+				selectionChanged(getStructuredSelection());
+			}
+
+			@Override
+			public boolean isPostcommitOnly() {
+				return true;
+			}
+		};
+	}
+
+	@Override
+	protected EObject getSelectedModelObject() {
+		return moduleConfiguration;
+	}
+
 	@Override
 	protected MetaModel getMetaModel() {
-		IModelDescriptor moduleDefModelDescriptor = ModelDescriptorRegistry.INSTANCE.getModel(moduleDefFile);
+		IFile moduleConfigurationFile = EcorePlatformUtil.getFile(moduleConfiguration);
+		IModelDescriptor moduleDefModelDescriptor = ModelDescriptorRegistry.INSTANCE.getModel(moduleConfigurationFile);
 		if (moduleDefModelDescriptor != null) {
 			return (MetaModel) moduleDefModelDescriptor.getAdapter(EcucMetaModel.class);
 		}
@@ -59,6 +128,7 @@ public class LaunchEcucCodeGenAction extends AbstractM2TAction {
 
 	@Override
 	protected String getQualifiedTemplateName() {
+		IFile moduleDefFile = EcorePlatformUtil.getFile(moduleDef);
 		return moduleDefFile.getProjectRelativePath().removeFileExtension().append(getRootDefineName()).toString().replaceAll("/", "::"); //$NON-NLS-1$ //$NON-NLS-2$
 	}
 
