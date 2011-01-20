b4bd31364180c52363fc41523106bb00dcbcb7c3
stephan.eberle <stephan.eberle@9c7074dd-d9a8-471e-8b89-2a72648c76b9>
2011-01-20 22:02:18 +0000

    Restored modifications related to ASSIGNED - bug 116: Extend Eclipse Refresh action in order to force model reload https://bugzilla01.geensys.com/show_bug.cgi?id=116

--- experimental/aal/plugins/org.artop.ecuc.codegen.xpand.ui/src/org/artop/ecuc/codegen/xpand/ui/actions/LaunchEcucCodeGenAction.java
+++ experimental/aal/plugins/org.artop.ecuc.codegen.xpand.ui/src/org/artop/ecuc/codegen/xpand/ui/actions/LaunchEcucCodeGenAction.java
@@ -3,27 +3,42 @@ package org.artop.ecuc.codegen.xpand.ui.actions;
 import gautosar.gecucdescription.GModuleConfiguration;
 import gautosar.gecucparameterdef.GModuleDef;
 
-import java.io.File;
-
 import org.artop.ecl.emf.model.IModelDescriptor;
 import org.artop.ecl.emf.model.ModelDescriptorRegistry;
 import org.artop.ecl.emf.util.EcorePlatformUtil;
+import org.artop.ecl.emf.workspace.loading.ModelLoadManager;
 import org.artop.ecl.platform.ui.util.ExtendedPlatformUI;
 import org.artop.ecuc.codegen.xpand.ui.internal.messages.Messages;
 import org.artop.ecuc.gautosar.xtend.typesystem.EcucMetaModel;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.transaction.NotificationFilter;
+import org.eclipse.emf.transaction.ResourceSetChangeEvent;
+import org.eclipse.emf.transaction.ResourceSetListener;
+import org.eclipse.emf.transaction.ResourceSetListenerImpl;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.emf.transaction.util.TransactionUtil;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.sphinx.xpand.ui.actions.AbstractM2TAction;
 import org.eclipse.sphinx.xpand.ui.wizards.M2TConfigurationWizard;
+import org.eclipse.xpand2.XpandUtil;
 import org.eclipse.xtend.typesystem.MetaModel;
 
 public class LaunchEcucCodeGenAction extends AbstractM2TAction {
 
 	public static final String DEFAULT_ROOT_DEFINE_NAME = "main"; //$NON-NLS-1$
+	public static final String UNKNOWN_TEMPLATE_NAME = "unknown"; //$NON-NLS-1$
 
-	protected IFile moduleConfigurationFile;
-	protected IFile moduleDefFile;
+	protected GModuleConfiguration moduleConfiguration;
+	protected GModuleDef moduleDef;
+
+	private ResourceSetListener resourceChangedListener = null;
 
 	public LaunchEcucCodeGenAction() {
 		super(Messages.menuItem_launchEcucCodeGen);
@@ -38,19 +53,76 @@ public class LaunchEcucCodeGenAction extends AbstractM2TAction {
 		if (selection.size() == 1) {
 			Object selected = selection.getFirstElement();
 			if (selected instanceof GModuleConfiguration) {
-				GModuleConfiguration moduleConfiguration = (GModuleConfiguration) selected;
-				moduleConfigurationFile = EcorePlatformUtil.getFile(moduleConfiguration);
-				GModuleDef moduleDefinition = moduleConfiguration.gGetDefinition();
-				moduleDefFile = EcorePlatformUtil.getFile(moduleDefinition);
-				return moduleConfigurationFile != null && moduleDefFile != null;
+				// Retrieve module configuration behind selection as well as module definition which it references
+				moduleConfiguration = (GModuleConfiguration) selected;
+				moduleDef = moduleConfiguration.gGetDefinition();
+
+				// Referenced module definition not available? (this can be the case when the AUTOSAR model behind the
+				// selected element has not been fully loaded yet or the referenced module definition is really
+				// missing)
+				if (moduleDef.eIsProxy()) {
+					// Trigger asynchronous loading of underlying AUTOSAR model including referenced AUTOSAR models in
+					// BSW Platform projects (i.e., AUTOSAR projects with Xpand/Xtend nature) to make sure that
+					// referenced module definition has a chance to get resolved
+					IFile moduleConfigurationFile = EcorePlatformUtil.getFile(moduleConfiguration);
+					IModelDescriptor moduleConfigurationModel = ModelDescriptorRegistry.INSTANCE.getModel(moduleConfigurationFile);
+					ModelLoadManager.INSTANCE.loadModel(moduleConfigurationModel, true, true, null);
+
+					// Install resource changed listener which updates this action's selection state once the loading
+					// has been finished
+					TransactionalEditingDomain editingDomain = TransactionUtil.getEditingDomain(moduleConfiguration);
+					if (editingDomain != null) {
+						if (resourceChangedListener == null) {
+							resourceChangedListener = createResourceChangedListener();
+						}
+						editingDomain.addResourceSetListener(resourceChangedListener);
+					}
+				} else {
+					// Uninstall resource changed listener in case there is any
+					if (resourceChangedListener != null) {
+						TransactionalEditingDomain editingDomain = TransactionUtil.getEditingDomain(moduleConfiguration);
+						if (editingDomain != null) {
+							editingDomain.removeResourceSetListener(resourceChangedListener);
+							resourceChangedListener = null;
+						}
+					}
+				}
+
+				return moduleDef != null && !moduleDef.eIsProxy();
 			}
 		}
 		return false;
 	}
 
+	/**
+	 * Creates a ResourceSetListener that detects (re-)loaded resources and updates this action's selection.
+	 */
+	protected ResourceSetListener createResourceChangedListener() {
+		return new ResourceSetListenerImpl(NotificationFilter
+				.createFeatureFilter(EcorePackage.eINSTANCE.getEResource(), Resource.RESOURCE__IS_LOADED).or(
+						NotificationFilter.createFeatureFilter(EcorePackage.eINSTANCE.getEResourceSet(), ResourceSet.RESOURCE_SET__RESOURCES))) {
+			@Override
+			public void resourceSetChanged(ResourceSetChangeEvent event) {
+				// Update this action's selection state
+				selectionChanged(getStructuredSelection());
+			}
+
+			@Override
+			public boolean isPostcommitOnly() {
+				return true;
+			}
+		};
+	}
+
+	@Override
+	protected EObject getSelectedModelObject() {
+		return moduleConfiguration;
+	}
+
 	@Override
 	protected MetaModel getMetaModel() {
-		IModelDescriptor moduleDefModelDescriptor = ModelDescriptorRegistry.INSTANCE.getModel(moduleDefFile);
+		IFile moduleConfigurationFile = EcorePlatformUtil.getFile(moduleConfiguration);
+		IModelDescriptor moduleDefModelDescriptor = ModelDescriptorRegistry.INSTANCE.getModel(moduleConfigurationFile);
 		if (moduleDefModelDescriptor != null) {
 			return (MetaModel) moduleDefModelDescriptor.getAdapter(EcucMetaModel.class);
 		}
@@ -59,7 +131,20 @@ public class LaunchEcucCodeGenAction extends AbstractM2TAction {
 
 	@Override
 	protected String getQualifiedTemplateName() {
-		return moduleDefFile.getProjectRelativePath().removeFileExtension().append(getRootDefineName()).toString().replaceAll("/", "::"); //$NON-NLS-1$ //$NON-NLS-2$
+		IFile templateFile = getTemplateFile();
+		if (templateFile != null) {
+			return templateFile.getProjectRelativePath().removeFileExtension().append(getRootDefineName()).toString().replaceAll("/", "::"); //$NON-NLS-1$ //$NON-NLS-2$
+		}
+		return UNKNOWN_TEMPLATE_NAME;
+	}
+
+	protected IFile getTemplateFile() {
+		IFile moduleDefFile = EcorePlatformUtil.getFile(moduleDef);
+		if (moduleDefFile != null) {
+			IPath templatePath = moduleDefFile.getFullPath().removeFileExtension().addFileExtension(XpandUtil.TEMPLATE_EXTENSION);
+			return ResourcesPlugin.getWorkspace().getRoot().getFile(templatePath);
+		}
+		return null;
 	}
 
 	protected String getRootDefineName() {
@@ -68,7 +153,7 @@ public class LaunchEcucCodeGenAction extends AbstractM2TAction {
 
 	@Override
 	public void run() {
-		if (!existsTemplate()) {
+		if (!existsTemplateFile()) {
 			M2TConfigurationWizard wizard = new M2TConfigurationWizard(getSelectedModelObject(), getMetaModel(), getOutletContainer());
 			WizardDialog wizardDialog = new WizardDialog(ExtendedPlatformUI.getDisplay().getActiveShell(), wizard);
 			wizardDialog.open();
@@ -77,7 +162,11 @@ public class LaunchEcucCodeGenAction extends AbstractM2TAction {
 		super.run();
 	}
 
-	protected boolean existsTemplate() {
-		return new File(moduleDefFile.getLocation().removeFileExtension().addFileExtension("xpt").toString()).exists(); //$NON-NLS-1$
+	protected boolean existsTemplateFile() {
+		IFile templateFile = getTemplateFile();
+		if (templateFile != null) {
+			return templateFile.exists();
+		}
+		return false;
 	}
 }
