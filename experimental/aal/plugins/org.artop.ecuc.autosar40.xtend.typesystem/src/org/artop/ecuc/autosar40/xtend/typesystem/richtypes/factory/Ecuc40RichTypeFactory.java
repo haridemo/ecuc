50e3c2aa357a363c18140879f445baa0ff2b852d
christian.knuechel <christian.knuechel@9c7074dd-d9a8-471e-8b89-2a72648c76b9>
2011-02-09 16:21:07 +0000

    Fixed copyright headers.

--- experimental/aal/plugins/org.artop.ecuc.autosar40.xtend.typesystem/src/org/artop/ecuc/autosar40/xtend/typesystem/richtypes/factory/Ecuc40RichTypeFactory.java
+++ experimental/aal/plugins/org.artop.ecuc.autosar40.xtend.typesystem/src/org/artop/ecuc/autosar40/xtend/typesystem/richtypes/factory/Ecuc40RichTypeFactory.java
@@ -1,3 +1,17 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) See4sys and others.
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Artop Software License Based on AUTOSAR
+ * Released Material (ASLR) which accompanies this distribution, and is
+ * available at http://www.artop.org/aslr.html
+ * 
+ * Contributors: 
+ *     See4sys - Initial API and implementation
+ * 
+ * </copyright>
+ */
 package org.artop.ecuc.autosar40.xtend.typesystem.richtypes.factory;
 
 import gautosar.gecucparameterdef.GBooleanParamDef;
