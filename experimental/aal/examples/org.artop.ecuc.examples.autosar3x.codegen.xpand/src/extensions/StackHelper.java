c1b78331d4adf9f9901223b92ece279532840767
aakar <aakar@9c7074dd-d9a8-471e-8b89-2a72648c76b9>
2011-02-18 14:15:12 +0000

    Added removed helpers.

---
+++ experimental/aal/examples/org.artop.ecuc.examples.autosar3x.codegen.xpand/src/extensions/StackHelper.java
@@ -0,0 +1,35 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) See4sys and others.
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Artop Software License Based on AUTOSAR
+ * Released Material (ASLR) which accompanies this distribution, and is
+ * available at http://www.artop.org/aslr.html
+ * 
+ * Contributors: 
+ *     See4sys - Initial API and implementation
+ * 
+ * </copyright>
+ */
+package extensions;
+
+import java.util.Stack;
+
+public final class StackHelper {
+
+	private final static Stack<Object> stack = new Stack<Object>();
+	
+	public static void push(Object obj) {
+		stack.push(obj);
+	}
+	
+	public static Object peek() {
+		return stack.peek();
+	}
+	
+	public static Object pop() {
+		return stack.pop();
+	}
+
+}
