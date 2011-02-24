fcc0d2a04585c591fbc20c1319157ba31de27171
christian.knuechel <christian.knuechel@9c7074dd-d9a8-471e-8b89-2a72648c76b9>
2011-02-24 18:49:13 +0000

    Removed warnings.

--- plugins/org.artop.ecuc.moduleconfiguration.initializer/src/org/artop/ecuc/moduleconfiguration/initializer/internal/ConfigurationConstants.java
+++ plugins/org.artop.ecuc.moduleconfiguration.initializer/src/org/artop/ecuc/moduleconfiguration/initializer/internal/ConfigurationConstants.java
@@ -13,25 +13,23 @@
  * </copyright>
  */
 package org.artop.ecuc.moduleconfiguration.initializer.internal;
+
 /**
- * 
- * Character Constants used 
- *
+ * Character Constants used
  */
+@SuppressWarnings("nls")
 public class ConfigurationConstants {
-	
+
 	public static final String EMPTY_STRING = "";
 	public static final String UPPER_MULTIPLICITY_MAX = "*";
-	
+
 	/* property ID */
 	public static final String PROPERTY_ID_SHORT_NAME = "shortName";
 	public static final String PROPERTY_ID_DEFINITION = "definition";
-	public static final String PROPERTY_ID_LOWER_MULTIPLICITY 
-	                                                    = "lowerMultiplicity";
-	public static final String PROPERTY_ID_UPPER_MULTIPLICITY 
-                                                        = "upperMultiplicity";
+	public static final String PROPERTY_ID_LOWER_MULTIPLICITY = "lowerMultiplicity";
+	public static final String PROPERTY_ID_UPPER_MULTIPLICITY = "upperMultiplicity";
 	public static final String PROPERTY_ID_VALUE = "value";
 	public static final String PROPERTY_ID_UUID = "uuid";
 	public static final String PROPERTY_ID_DEFAULT_VALUE = "defaultValue";
-	
+
 }
--- plugins/org.artop.ecuc.moduleconfiguration.initializer/src/org/artop/ecuc/moduleconfiguration/initializer/internal/DescriptionDefinitionMap.java
+++ plugins/org.artop.ecuc.moduleconfiguration.initializer/src/org/artop/ecuc/moduleconfiguration/initializer/internal/DescriptionDefinitionMap.java
@@ -19,68 +19,62 @@ import java.util.HashMap;
 /**
  * This class is used to keep the definition description map.
  */
+@SuppressWarnings("nls")
 public class DescriptionDefinitionMap {
-    
-    /**
-     * Map for keeping the definition, description class names
-     */
-    private static HashMap<String, String> definitionDescriptionMap = 
-                                                new HashMap<String, String>();
-    
-    /**
-     * Constructor
-     */
-    private DescriptionDefinitionMap() {
-    }
-    
-    /**
-     * Map for keeping the definition and description values
-     */
-    static {
-        definitionDescriptionMap.put("ARPackage", "ARPackage");
-        definitionDescriptionMap.put("ModuleDef", "ModuleConfiguration");
-        definitionDescriptionMap.put("ParamConfContainerDef", "Container");
-        definitionDescriptionMap.put("BooleanParamDef", "BooleanValue");
-        definitionDescriptionMap.put("EnumerationParamDef", "EnumerationValue");
-        definitionDescriptionMap.put("FloatParamDef", "FloatValue");
-        definitionDescriptionMap.put("IntegerParamDef", "IntegerValue");
-        definitionDescriptionMap.put("StringParamDef", "StringValue");
-        definitionDescriptionMap.put("FunctionNameDef", "FunctionNameValue");
-        definitionDescriptionMap.put("ReferenceParamDef", "ReferenceValue");
-        definitionDescriptionMap.put("LinkerSymbolDef", "LinkerSymbolValue");
-        definitionDescriptionMap.put("DerivedStringParamDef", "StringValue");
-        definitionDescriptionMap.put("DerivedIntegerParamDef", "IntegerValue");
-        definitionDescriptionMap.put("DerivedFloatParamDef", "FloatValue");
-        definitionDescriptionMap.put("DerivedEnumerationParamDef", 
-                                     "EnumerationValue");
-        definitionDescriptionMap.put("DerivedBooleanParamDef", "BooleanValue");
-        definitionDescriptionMap.put("SymbolicNameReferenceParamDef", 
-                                     "ReferenceValue");
-        definitionDescriptionMap.put("ChoiceContainerDef", "Container");
-        definitionDescriptionMap.put("ChoiceReferenceParamDef", 
-                                     "ReferenceValue");
-        definitionDescriptionMap.put("ConfigReference", "ConfigReferenceValue");
-        definitionDescriptionMap.put("ForeignReferenceParamDef", 
-                                     "ReferenceValue");
-        definitionDescriptionMap.put("InstanceReferenceParamDef", 
-                                     "InstanceReferenceValue");
-        definitionDescriptionMap.put("ReferenceParamDef", "ReferenceValue");
-        definitionDescriptionMap.put("Short Name", "Short Name");
-        
-    }
-    
-    /**
-     * For getting the description node name for a particular 
-     * definition node.
-     * 
-     * @param definitionName
-     *            The name of the definition node.
-     */
-    public static String getDescriptionName(String definitionName) {
-        if (definitionDescriptionMap.containsKey(definitionName)) {
-            return definitionDescriptionMap.get(definitionName);
-        }
-        return ConfigurationConstants.EMPTY_STRING;
-    }
-    
+
+	/**
+	 * Map for keeping the definition, description class names
+	 */
+	private static HashMap<String, String> definitionDescriptionMap = new HashMap<String, String>();
+
+	/**
+	 * Constructor
+	 */
+	private DescriptionDefinitionMap() {
+	}
+
+	/**
+	 * Map for keeping the definition and description values
+	 */
+	static {
+		definitionDescriptionMap.put("ARPackage", "ARPackage");
+		definitionDescriptionMap.put("ModuleDef", "ModuleConfiguration");
+		definitionDescriptionMap.put("ParamConfContainerDef", "Container");
+		definitionDescriptionMap.put("BooleanParamDef", "BooleanValue");
+		definitionDescriptionMap.put("EnumerationParamDef", "EnumerationValue");
+		definitionDescriptionMap.put("FloatParamDef", "FloatValue");
+		definitionDescriptionMap.put("IntegerParamDef", "IntegerValue");
+		definitionDescriptionMap.put("StringParamDef", "StringValue");
+		definitionDescriptionMap.put("FunctionNameDef", "FunctionNameValue");
+		definitionDescriptionMap.put("ReferenceParamDef", "ReferenceValue");
+		definitionDescriptionMap.put("LinkerSymbolDef", "LinkerSymbolValue");
+		definitionDescriptionMap.put("DerivedStringParamDef", "StringValue");
+		definitionDescriptionMap.put("DerivedIntegerParamDef", "IntegerValue");
+		definitionDescriptionMap.put("DerivedFloatParamDef", "FloatValue");
+		definitionDescriptionMap.put("DerivedEnumerationParamDef", "EnumerationValue");
+		definitionDescriptionMap.put("DerivedBooleanParamDef", "BooleanValue");
+		definitionDescriptionMap.put("SymbolicNameReferenceParamDef", "ReferenceValue");
+		definitionDescriptionMap.put("ChoiceContainerDef", "Container");
+		definitionDescriptionMap.put("ChoiceReferenceParamDef", "ReferenceValue");
+		definitionDescriptionMap.put("ConfigReference", "ConfigReferenceValue");
+		definitionDescriptionMap.put("ForeignReferenceParamDef", "ReferenceValue");
+		definitionDescriptionMap.put("InstanceReferenceParamDef", "InstanceReferenceValue");
+		definitionDescriptionMap.put("ReferenceParamDef", "ReferenceValue");
+		definitionDescriptionMap.put("Short Name", "Short Name");
+
+	}
+
+	/**
+	 * For getting the description node name for a particular definition node.
+	 * 
+	 * @param definitionName
+	 *            The name of the definition node.
+	 */
+	public static String getDescriptionName(String definitionName) {
+		if (definitionDescriptionMap.containsKey(definitionName)) {
+			return definitionDescriptionMap.get(definitionName);
+		}
+		return ConfigurationConstants.EMPTY_STRING;
+	}
+
 }
