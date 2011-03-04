35c2be24ab22793d16513325568d9a012ed4666b
yannick.didierjean <yannick.didierjean@9c7074dd-d9a8-471e-8b89-2a72648c76b9>
2011-03-04 17:27:15 +0000

    Implementation of property swVersion on RichModuleDef40Type

--- experimental/aal/plugins/org.artop.ecuc.autosar40.xtend.typesystem/src/org/artop/ecuc/autosar40/xtend/typesystem/richtypes/impl/RichModuleDef40TypeImpl.java
+++ experimental/aal/plugins/org.artop.ecuc.autosar40.xtend.typesystem/src/org/artop/ecuc/autosar40/xtend/typesystem/richtypes/impl/RichModuleDef40TypeImpl.java
@@ -5,8 +5,10 @@ import gautosar.gecucparameterdef.GModuleDef;
 import org.artop.ecuc.gautosar.xtend.typesystem.EcucContext;
 import org.artop.ecuc.gautosar.xtend.typesystem.richtypes.impl.RichModuleDefTypeImpl;
 import org.eclipse.internal.xtend.type.baseimpl.OperationImpl;
+import org.eclipse.internal.xtend.type.baseimpl.PropertyImpl;
 import org.eclipse.xtend.typesystem.Type;
 
+import autosar40.bswmodule.bswimplementation.BswImplementation;
 import autosar40.ecucdescription.EcucModuleConfigurationValues;
 import autosar40.ecucparameterdef.EcucConfigurationVariantEnum;
 
@@ -19,6 +21,19 @@ public class RichModuleDef40TypeImpl extends RichModuleDefTypeImpl {
 	@Override
 	protected void addBaseFeatures() {
 		super.addBaseFeatures();
+		addFeature(new PropertyImpl(this, "swVersion", getTypeSystem().getStringType()) { //$NON-NLS-1$
+			public Object get(Object target) {
+				if (target instanceof EcucModuleConfigurationValues) {
+					EcucModuleConfigurationValues ecucModuleConfigurationValues = (EcucModuleConfigurationValues) target;
+					BswImplementation moduleDescription = ecucModuleConfigurationValues.getModuleDescription();
+					if (moduleDescription != null) {
+						return moduleDescription.getSwVersion();
+					}
+				}
+				return "";
+			}
+
+		});
 		addFeature(new OperationImpl(this, "isPreCompile", getTypeSystem().getBooleanType(), new Type[0]) { //$NON-NLS-1$
 			@Override
 			protected Object evaluateInternal(Object target, Object[] params) {
