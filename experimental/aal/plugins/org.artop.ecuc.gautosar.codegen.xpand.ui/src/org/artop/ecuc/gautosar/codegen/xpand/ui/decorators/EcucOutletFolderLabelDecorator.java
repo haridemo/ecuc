c8e09d9d2e22643f709280ca4ad61334cbdec7cb
stephan.eberle <stephan.eberle@9c7074dd-d9a8-471e-8b89-2a72648c76b9>
2011-03-29 12:48:39 +0000

    Added ECUC Outlet Folder Decorator

---
+++ experimental/aal/plugins/org.artop.ecuc.gautosar.codegen.xpand.ui/src/org/artop/ecuc/gautosar/codegen/xpand/ui/decorators/EcucOutletFolderLabelDecorator.java
@@ -0,0 +1,27 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) See4sys and others.
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Artop Software License Based on AUTOSAR
+ * Released Material (ASLR) which accompanies this distribution, and is
+ * available at http://www.artop.org/aslr.html
+ * 
+ * Contributors: 
+ *     See4sys - Initial API and implementation
+ * 
+ * </copyright>
+ */
+package org.artop.ecuc.gautosar.codegen.xpand.ui.decorators;
+
+import org.artop.ecuc.gautosar.codegen.xpand.ui.preferences.IEcucCodeGenerationPreferenceConstants;
+import org.eclipse.sphinx.xpand.preferences.OutletsPreference;
+import org.eclipse.sphinx.xpand.ui.decorators.BasicOutletFolderLabelDecorator;
+
+public class EcucOutletFolderLabelDecorator extends BasicOutletFolderLabelDecorator {
+
+	@Override
+	protected OutletsPreference getOutletsPreference() {
+		return IEcucCodeGenerationPreferenceConstants.ECUC_OUTLETS_PREFERENCE;
+	}
+}
