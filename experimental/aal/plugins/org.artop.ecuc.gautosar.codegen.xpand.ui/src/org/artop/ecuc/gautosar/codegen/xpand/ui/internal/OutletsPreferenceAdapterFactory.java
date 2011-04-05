5a9c18237419f15c11b09d9afde4abcbd0206fd3
idrissa.dieng <idrissa.dieng@9c7074dd-d9a8-471e-8b89-2a72648c76b9>
2011-04-05 17:03:41 +0000

    Adds an outlet preference adapter factory.

---
+++ experimental/aal/plugins/org.artop.ecuc.gautosar.codegen.xpand.ui/src/org/artop/ecuc/gautosar/codegen/xpand/ui/internal/OutletsPreferenceAdapterFactory.java
@@ -0,0 +1,47 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) See4sys and others.
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Artop Software License Based on AUTOSAR
+ * Released Material (ASLR) which accompanies this distribution, and is
+ * available at http://www.artop.org/aslr.html
+ * 
+ * Contributors: 
+ *     See4sys - Initial API and implementation
+ * 
+ * </copyright>
+ */
+package org.artop.ecuc.gautosar.codegen.xpand.ui.internal;
+
+import org.artop.ecuc.gautosar.codegen.xpand.ui.preferences.EcucCodeGenerationPreferencePage;
+import org.eclipse.core.runtime.IAdapterFactory;
+import org.eclipse.sphinx.platform.ui.preferences.IPropertyPageIdProvider;
+import org.eclipse.sphinx.xpand.preferences.OutletsPreference;
+
+public class OutletsPreferenceAdapterFactory implements IAdapterFactory {
+
+	/*
+	 * @see org.eclipse.core.runtime.IAdapterFactory#getAdapter(java.lang.Object, java.lang.Class)
+	 */
+	public Object getAdapter(final Object adaptableObject, @SuppressWarnings("rawtypes") Class adapterType) {
+		if (adapterType.equals(OutletsPreference.class)) {
+			if (adaptableObject instanceof IPropertyPageIdProvider) {
+				return new IPropertyPageIdProvider() {
+					public String getPropertyPageId() {
+						return EcucCodeGenerationPreferencePage.PROP_PAGE_ID;
+					}
+				};
+			}
+		}
+		return null;
+	}
+
+	/*
+	 * @see org.eclipse.core.runtime.IAdapterFactory#getAdapterList()
+	 */
+	@SuppressWarnings("rawtypes")
+	public Class[] getAdapterList() {
+		return new Class<?>[] { IPropertyPageIdProvider.class };
+	}
+}
