17fcc4fee027ac55a1de51c82dcd591fb05f422f
seberle <seberle@9c7074dd-d9a8-471e-8b89-2a72648c76b9>
2011-09-19 19:05:01 +0000

    ASSIGNED - 1448: Provide an example for automatically creating ECU configurations from ECU extracts of system descriptions using Xtend https://www.artop.org/bugs/show_bug.cgi?id=1448

--- aal/examples/org.artop.ecuc.examples.autosar40.transform.xtend.ui/src/org/artop/ecuc/examples/autosar40/transform/xtend/ui/actions/LaunchEcucAutoConfigure40Action.java
+++ aal/examples/org.artop.ecuc.examples.autosar40.transform.xtend.ui/src/org/artop/ecuc/examples/autosar40/transform/xtend/ui/actions/LaunchEcucAutoConfigure40Action.java
@@ -15,31 +15,12 @@
  */
 package org.artop.ecuc.examples.autosar40.transform.xtend.ui.actions;
 
-import gautosar.ggenericstructure.ginfrastructure.GARPackage;
-import gautosar.ggenericstructure.ginfrastructure.GAUTOSAR;
-import gautosar.ggenericstructure.ginfrastructure.GPackageableElement;
-
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 import org.artop.aal.common.metamodel.AutosarReleaseDescriptor;
-import org.artop.aal.gautosar.services.DefaultMetaModelServiceProvider;
-import org.artop.aal.gautosar.services.factories.IGAutosarFactoryService;
 import org.artop.ecuc.examples.autosar40.transform.xtend.ui.internal.messages.Messages;
-import org.eclipse.core.resources.IFile;
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.jobs.IJobChangeListener;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.jface.wizard.WizardDialog;
-import org.eclipse.sphinx.emf.metamodel.IMetaModelDescriptor;
-import org.eclipse.sphinx.emf.metamodel.MetaModelDescriptorRegistry;
-import org.eclipse.sphinx.emf.util.EcorePlatformUtil;
 import org.eclipse.sphinx.platform.ui.util.ExtendedPlatformUI;
-import org.eclipse.sphinx.xtendxpand.jobs.SaveAsNewFileHandler;
 import org.eclipse.sphinx.xtendxpand.jobs.XtendJob;
 import org.eclipse.sphinx.xtendxpand.ui.actions.BasicM2MAction;
 import org.eclipse.sphinx.xtendxpand.ui.wizards.M2MConfigurationWizard;
@@ -62,82 +43,9 @@ public class LaunchEcucAutoConfigure40Action extends BasicM2MAction {
 		M2MConfigurationWizard wizard = new M2MConfigurationWizard(getSelectedModelObject(), getMetaModels()) {
 			@Override
 			protected IJobChangeListener createResultObjectHandler(XtendJob xtendJob) {
-				return new SaveAsNewFileHandler(xtendJob) {
-
-					private String rootARPackageName = "ARRoot"; //$NON-NLS-1$
-					private IPath projectRelativeResultPath = new Path("bsw.config").append("Car_Configuration").addFileExtension(AutosarReleaseDescriptor.ARXML_DEFAULT_FILE_EXTENSION); //$NON-NLS-1$ //$NON-NLS-2$
-
-					@Override
-					protected void handleResultObjects(Map<Object, Collection<?>> resultObjects) {
-						DefaultMetaModelServiceProvider mmServiceProvider = new DefaultMetaModelServiceProvider();
-						Map<Object, Collection<?>> saveableResultObjects = new HashMap<Object, Collection<?>>();
-
-						for (Object inputObject : resultObjects.keySet()) {
-							Collection<?> resultObjectsForInputObject = resultObjects.get(inputObject);
-							Set<Object> saveableResultObjectForInputObject = new HashSet<Object>();
-
-							if (!resultObjectsForInputObject.isEmpty()) {
-								GARPackage rootARPackage = null;
-								GAUTOSAR autosar = null;
-								IMetaModelDescriptor mmDescriptor = MetaModelDescriptorRegistry.INSTANCE.getDescriptor(resultObjectsForInputObject
-										.iterator().next());
-								IGAutosarFactoryService factory = mmServiceProvider.getService(mmDescriptor, IGAutosarFactoryService.class);
-								if (factory != null) {
-									autosar = factory.createGAUTOSAR();
-									rootARPackage = factory.createGARPackage();
-									rootARPackage.gSetShortName(getNewRootARPackageName());
-									autosar.gGetArPackages().add(rootARPackage);
-								}
-
-								for (Object resultObject : resultObjectsForInputObject) {
-									if (resultObject instanceof GAUTOSAR) {
-										saveableResultObjectForInputObject.add(resultObject);
-									} else if (resultObject instanceof GPackageableElement) {
-										if (rootARPackage != null) {
-											rootARPackage.gGetElements().add((GPackageableElement) resultObject);
-										}
-									}
-								}
-
-								if (rootARPackage != null && !rootARPackage.gGetElements().isEmpty()) {
-									saveableResultObjectForInputObject.add(autosar);
-								}
-							}
-
-							if (!saveableResultObjectForInputObject.isEmpty()) {
-								saveableResultObjects.put(inputObject, saveableResultObjectForInputObject);
-							}
-						}
-
-						super.handleResultObjects(saveableResultObjects);
-					}
-
-					public String getNewRootARPackageName() {
-						return rootARPackageName;
-					}
-
-					public void setNewRootARPackageName(String rootARPackageName) {
-						this.rootARPackageName = rootARPackageName;
-
-					}
-
-					public IPath getProjectRelativeResultPath() {
-						return projectRelativeResultPath;
-					}
-
-					public void setProjectRelativeResultPath(IPath projectRelativeResultPath) {
-						this.projectRelativeResultPath = projectRelativeResultPath;
-					}
-
-					@Override
-					protected IPath getResultPath(Object inputObject, EObject resultObject) {
-						IFile inputFile = EcorePlatformUtil.getFile(inputObject);
-						if (inputFile != null) {
-							return inputFile.getProject().getFullPath().append(getProjectRelativeResultPath());
-						}
-						return null;
-					}
-				};
+				SaveAsNewAutosarFileHandler handler = new SaveAsNewAutosarFileHandler(xtendJob);
+				handler.setDefaultProjectRelativeResultPath(new Path("bsw.config").append("Car_Configuration").addFileExtension(AutosarReleaseDescriptor.ARXML_DEFAULT_FILE_EXTENSION)); //$NON-NLS-1$ //$NON-NLS-2$
+				return handler;
 			}
 		};
 		wizard.setM2MJobName(getM2MJobName());
---
+++ aal/examples/org.artop.ecuc.examples.autosar40.transform.xtend.ui/src/org/artop/ecuc/examples/autosar40/transform/xtend/ui/actions/SaveAsNewAutosarFileHandler.java
@@ -0,0 +1,136 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) itemis and others.
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Artop Software License Based on AUTOSAR
+ * Released Material (ASLR) which accompanies this distribution, and is
+ * available at http://www.artop.org/aslr.html
+ * 
+ * Contributors: 
+ *     itemis - Initial API and implementation
+ * 
+ * </copyright>
+ */
+package org.artop.ecuc.examples.autosar40.transform.xtend.ui.actions;
+
+import gautosar.ggenericstructure.ginfrastructure.GARPackage;
+import gautosar.ggenericstructure.ginfrastructure.GAUTOSAR;
+import gautosar.ggenericstructure.ginfrastructure.GPackageableElement;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.artop.aal.gautosar.services.DefaultMetaModelServiceProvider;
+import org.artop.aal.gautosar.services.factories.IGAutosarFactoryService;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.jobs.IJobChangeListener;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.sphinx.emf.metamodel.IMetaModelDescriptor;
+import org.eclipse.sphinx.emf.metamodel.MetaModelDescriptorRegistry;
+import org.eclipse.sphinx.xtendxpand.jobs.SaveAsNewFileHandler;
+import org.eclipse.sphinx.xtendxpand.jobs.XtendJob;
+
+import autosar40.autosartoplevelstructure.AUTOSAR;
+import autosar40.genericstructure.generaltemplateclasses.arpackage.ARPackage;
+import autosar40.genericstructure.generaltemplateclasses.arpackage.PackageableElement;
+
+/**
+ * An {@link IJobChangeListener} implementation that can be registered on an {@link XtendJob} instance or a {@link Job}
+ * instance that encloses the latter and saves the {@link XtendJob#getResultObjects() result objects} produced by the
+ * {@link XtendJob} as new AUTOSAR XML files in the workspace. Supports result objects being {@link AUTOSAR}s or
+ * {@link PackageableElement}s to be saved directly. While {@link AUTOSAR} objects are saved in new AUTOSAR XML files as
+ * is {@link PackageableElement} objects are automatically wrapped in a new {@link AUTOSAR} object and an
+ * {@link ARPackage} with a {@link ARPackage#getShortName() short name} computed by
+ * {@link #getResultARPackageName(Object)}.
+ * 
+ * @see XtendJob
+ */
+public class SaveAsNewAutosarFileHandler extends SaveAsNewFileHandler {
+
+	protected static final String DEFAULT_ROOT_ARPACKAGE_NAME = "ARoot"; //$NON-NLS-1$
+
+	private String defaultResultARPackageName = null;
+	private IPath defaultProjectRelativeResultPath = null;
+
+	public SaveAsNewAutosarFileHandler(XtendJob xtendJob) {
+		super(xtendJob);
+	}
+
+	public String getDefaultResultARPackageName() {
+		if (defaultResultARPackageName == null) {
+			defaultResultARPackageName = DEFAULT_ROOT_ARPACKAGE_NAME;
+		}
+		return defaultResultARPackageName;
+	}
+
+	public void setDefaultResultARPackageName(String defaultResultARPackageName) {
+		this.defaultResultARPackageName = defaultResultARPackageName;
+	}
+
+	public IPath getDefaultPRojectRelativeResultPath() {
+		return defaultProjectRelativeResultPath;
+	}
+
+	public void setDefaultProjectRelativeResultPath(IPath defaultProjectRelativeResultPath) {
+		this.defaultProjectRelativeResultPath = defaultProjectRelativeResultPath;
+	}
+
+	protected String getResultARPackageName(Object inputObject) {
+		return defaultResultARPackageName;
+	}
+
+	@Override
+	protected IPath getProjectRelativeResultPath(IFile inputFile, EObject resultObject) {
+		return defaultProjectRelativeResultPath;
+	}
+
+	@Override
+	protected void handleResultObjects(Map<Object, Collection<?>> resultObjects) {
+		DefaultMetaModelServiceProvider mmServiceProvider = new DefaultMetaModelServiceProvider();
+		Map<Object, Collection<?>> saveableResultObjects = new HashMap<Object, Collection<?>>();
+
+		for (Object inputObject : resultObjects.keySet()) {
+			Collection<?> resultObjectsForInputObject = resultObjects.get(inputObject);
+			Set<Object> saveableResultObjectsForInputObject = new HashSet<Object>();
+
+			if (!resultObjectsForInputObject.isEmpty()) {
+				GARPackage resultARPackage = null;
+				GAUTOSAR resultAutosar = null;
+				IMetaModelDescriptor mmDescriptor = MetaModelDescriptorRegistry.INSTANCE.getDescriptor(resultObjectsForInputObject.iterator().next());
+				IGAutosarFactoryService factory = mmServiceProvider.getService(mmDescriptor, IGAutosarFactoryService.class);
+				if (factory != null) {
+					resultAutosar = factory.createGAUTOSAR();
+					resultARPackage = factory.createGARPackage();
+					resultARPackage.gSetShortName(getResultARPackageName(inputObject));
+					resultAutosar.gGetArPackages().add(resultARPackage);
+				}
+
+				for (Object resultObject : resultObjectsForInputObject) {
+					if (resultObject instanceof GAUTOSAR) {
+						saveableResultObjectsForInputObject.add(resultObject);
+					} else if (resultObject instanceof GPackageableElement) {
+						if (resultARPackage != null) {
+							resultARPackage.gGetElements().add((GPackageableElement) resultObject);
+						}
+					}
+				}
+
+				if (resultARPackage != null && !resultARPackage.gGetElements().isEmpty()) {
+					saveableResultObjectsForInputObject.add(resultAutosar);
+				}
+			}
+
+			if (!saveableResultObjectsForInputObject.isEmpty()) {
+				saveableResultObjects.put(inputObject, saveableResultObjectsForInputObject);
+			}
+		}
+
+		super.handleResultObjects(saveableResultObjects);
+	}
+}
