269085e8e457dea2c62abaad9da331c6ab88c893
seberle <seberle@9c7074dd-d9a8-471e-8b89-2a72648c76b9>
2011-10-05 16:25:41 +0000

    Adapted to changes in Sphinx

--- plugins/org.artop.ecuc.gautosar.codegen.xpand.ui/src/org/artop/ecuc/gautosar/codegen/xpand/ui/actions/LaunchEcucCodeGenAction.java
+++ plugins/org.artop.ecuc.gautosar.codegen.xpand.ui/src/org/artop/ecuc/gautosar/codegen/xpand/ui/actions/LaunchEcucCodeGenAction.java
@@ -1,7 +1,7 @@
 /**
  * <copyright>
  * 
- * Copyright (c) See4sys and others.
+ * Copyright (c) See4sys, itemis and others.
  * All rights reserved. This program and the accompanying materials are made
  * available under the terms of the Artop Software License Based on AUTOSAR
  * Released Material (ASLR) which accompanies this distribution, and is
@@ -9,6 +9,7 @@
  * 
  * Contributors: 
  *     See4sys - Initial API and implementation
+ *     itemis - Adapted to changes in Sphinx
  * 
  * </copyright>
  */
@@ -140,6 +141,8 @@ public class LaunchEcucCodeGenAction extends BasicM2TAction {
 		wizard.setWorkspaceResourceLoader(getWorkspaceResourceLoader());
 		wizard.setOutletsPreference(getOutletsPreference());
 		wizard.setDefaultOutlet(getDefaultOutlet());
+		wizard.setResultMessageHandler(createResultMessageHandler());
+
 		WizardDialog wizardDialog = new WizardDialog(ExtendedPlatformUI.getDisplay().getActiveShell(), wizard);
 		wizardDialog.open();
 	}
