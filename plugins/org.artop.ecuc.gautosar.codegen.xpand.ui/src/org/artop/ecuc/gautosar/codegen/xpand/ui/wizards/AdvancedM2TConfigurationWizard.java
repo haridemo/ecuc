0507a16f96d6cf0425b4cfbd0c5d669c25911814
romain.sezestre <romain.sezestre@9c7074dd-d9a8-471e-8b89-2a72648c76b9>
2012-03-09 10:42:42 +0000

    RESOLVED - bug 1574: [ECUC Xpand UI] M2T operation failed for non-existing directory in outlet with protected region https://www.artop.org/bugs/show_bug.cgi?id=1574
    Fixed by creating ExtendedM2TConfigurationWizard which overrides getPrSrcPaths for creating outlet directories that do not already exist (when protected region is set to true).

--- plugins/org.artop.ecuc.gautosar.codegen.xpand.ui/src/org/artop/ecuc/gautosar/codegen/xpand/ui/wizards/AdvancedM2TConfigurationWizard.java
+++ plugins/org.artop.ecuc.gautosar.codegen.xpand.ui/src/org/artop/ecuc/gautosar/codegen/xpand/ui/wizards/AdvancedM2TConfigurationWizard.java
@@ -24,11 +24,11 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.sphinx.emf.util.EcorePlatformUtil;
 import org.eclipse.sphinx.xtendxpand.CheckEvaluationRequest;
 import org.eclipse.sphinx.xtendxpand.jobs.CheckJob;
-import org.eclipse.sphinx.xtendxpand.ui.wizards.M2TConfigurationWizard;
 import org.eclipse.sphinx.xtendxpand.ui.wizards.pages.XpandConfigurationPage;
 import org.eclipse.xtend.typesystem.MetaModel;
 
-public class AdvancedM2TConfigurationWizard extends M2TConfigurationWizard {
+// FIXME Make this class inherit from M2TConfigurationWizard (as soon as Sphinx creates non-existing outlet directory when protected region is set to true).
+public class AdvancedM2TConfigurationWizard extends ExtendedM2TConfigurationWizard {
 
 	public AdvancedM2TConfigurationWizard(EObject modelObject, List<MetaModel> metaModels) {
 		super(modelObject, metaModels);
