220c41c930e4d8dab458d263a9b9159f945affcc
idieng <idieng@9c7074dd-d9a8-471e-8b89-2a72648c76b9>
2012-03-22 16:56:16 +0000

    In progress https://www.artop.org/bugs/show_bug.cgi?id=1582

---
+++ aal/plugins/org.artop.ecuc.autosar3x.xtend.typesystem/src/org/artop/ecuc/autosar3x/xtend/typesystem/metatypes/concrete/EcuParameterDefinitionType.java
@@ -0,0 +1,22 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) itemis and others.
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Artop Software License Based on AUTOSAR
+ * Released Material (ASLR) which accompanies this distribution, and is
+ * available at http://www.artop.org/aslr.html
+ * 
+ * Contributors: 
+ *     itemis - Initial API and implementation
+ * 
+ * </copyright>
+ */
+package org.artop.ecuc.autosar3x.xtend.typesystem.metatypes.concrete;
+
+import org.artop.ecuc.gautosar.xtend.typesystem.metatypes.EcucMetaType;
+
+public interface EcuParameterDefinitionType extends EcucMetaType {
+
+	String TYPE_NAME = META_NAMESPACE + TYPE_NAME_SEGMENT_SEPARATOR + "EcucParameterDefinitionType"; //$NON-NLS-1$
+}
