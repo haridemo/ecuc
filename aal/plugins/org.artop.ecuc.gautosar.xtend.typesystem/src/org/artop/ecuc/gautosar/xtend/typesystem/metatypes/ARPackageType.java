0a1f80619daba15dafd570ed0bc1a488d4e5600a
idieng <idieng@9c7074dd-d9a8-471e-8b89-2a72648c76b9>
2012-03-22 16:35:39 +0000

    In progress https://www.artop.org/bugs/show_bug.cgi?id=1582

---
+++ aal/plugins/org.artop.ecuc.gautosar.xtend.typesystem/src/org/artop/ecuc/gautosar/xtend/typesystem/metatypes/ARPackageType.java
@@ -0,0 +1,20 @@
+/**
+ * <copyright>
+ * 
+ * Copyright (c) See4sys and others.
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Artop Software License Based on AUTOSAR
+ * Released Material (ASLR) which accompanies this distribution, and is
+ * available at http://www.artop.org/aslr.html
+ * 
+ * Contributors: 
+ *     See4sys - Initial API and implementation
+ * 
+ * </copyright>
+ */
+package org.artop.ecuc.gautosar.xtend.typesystem.metatypes;
+
+public interface ARPackageType extends EcucMetaType {
+
+	String TYPE_NAME = META_NAMESPACE + TYPE_NAME_SEGMENT_SEPARATOR + "ARPackageType"; //$NON-NLS-1$
+}
