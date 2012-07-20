f08a5bbf53eaf47d1966f9845655b0b073751060
romain.sezestre <romain.sezestre@9c7074dd-d9a8-471e-8b89-2a72648c76b9>
2012-07-20 12:17:14 +0000

    RESOLVED - bug 1638: Unsupported big integer values in Ecuc Typesystem https://www.artop.org/bugs/show_bug.cgi?id=1638
    Updated test accordingly.

--- aal/tests/org.artop.ecuc.autosar40.typesystem.integration.tests/src/org/artop/ecuc/autosar40/typesystem/integration/tests/EcucMetaModelTest.java
+++ aal/tests/org.artop.ecuc.autosar40.typesystem.integration.tests/src/org/artop/ecuc/autosar40/typesystem/integration/tests/EcucMetaModelTest.java
@@ -14,6 +14,7 @@
  */
 package org.artop.ecuc.autosar40.typesystem.integration.tests;
 
+import java.math.BigInteger;
 import java.util.List;
 
 import org.artop.aal.common.resource.AutosarURIFactory;
@@ -889,8 +890,8 @@ public class EcucMetaModelTest extends AbstractEcucIntegrationTestCase {
 		assertNotNull(valueProperty);
 		assertEquals(ecucMetaModel.getTypeSystem().getIntegerType(), valueProperty.getReturnType());
 		Object value = valueProperty.get(serialNumberObject);
-		assertTrue(value instanceof Integer);
-		assertTrue(((Integer) value).intValue() == 879055672);
+		assertTrue(value instanceof BigInteger);
+		assertTrue(((BigInteger) value).intValue() == 879055672);
 		/********************************************************************/
 
 		/** 34_2_1 Operation is_configured **/
