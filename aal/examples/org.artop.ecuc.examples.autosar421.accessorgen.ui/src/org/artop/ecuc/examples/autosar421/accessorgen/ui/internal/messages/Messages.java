cca0adc16f6c02c8e2e9fb04ba18599f2c5cc713
Idrissa Dieng <idrissa.dieng@itemis.com>
2014-11-28 11:46:37 +0100

    Renamed Ecuc value example UI plugin https://www.artop.org/bugs/show_bug.cgi?id=2016

--- aal/examples/org.artop.ecuc.examples.autosar421.accessorgen.ui/src/org/artop/ecuc/examples/autosar421/accessorgen/ui/internal/messages/Messages.java
+++ aal/examples/org.artop.ecuc.examples.autosar421.accessorgen.ui/src/org/artop/ecuc/examples/autosar421/accessorgen/ui/internal/messages/Messages.java
@@ -1,15 +1,15 @@
 /**
  * <copyright>
- * 
+ *
  * Copyright (c) itemis and others.
  * All rights reserved. This program and the accompanying materials are made
  * available under the terms of the Artop Software License Based on AUTOSAR
  * Released Material (ASLR) which accompanies this distribution, and is
  * available at http://www.artop.org/aslr.html
- * 
- * Contributors: 
+ *
+ * Contributors:
  *     itemis - Initial API and implementation
- * 
+ *
  * </copyright>
  */
 package org.artop.ecuc.examples.autosar421.accessorgen.ui.internal.messages;
@@ -18,7 +18,7 @@ import org.eclipse.osgi.util.NLS;
 
 public class Messages extends NLS {
 
-	private static final String BUNDLE_NAME = "org.artop.ecuc.examples.autosar4x.accessorgen.ui.internal.messages.messages"; //$NON-NLS-1$
+	private static final String BUNDLE_NAME = "org.artop.ecuc.examples.autosar421.accessorgen.ui.internal.messages.messages"; //$NON-NLS-1$
 
 	public static String menu_ecucValueAccessorGen_label;
 
