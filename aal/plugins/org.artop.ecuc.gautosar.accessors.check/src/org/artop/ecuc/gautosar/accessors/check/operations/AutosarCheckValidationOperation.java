f94cf17cdafdbc9c7e6546fa7bec42ae4a599d3a
seberle <stephaneberle9@gmail.com>
2015-08-28 23:28:59 +0200

    Adapted AutosarCheckValidationOperation to changed Sphinx API

--- aal/plugins/org.artop.ecuc.gautosar.accessors.check/src/org/artop/ecuc/gautosar/accessors/check/operations/AutosarCheckValidationOperation.java
+++ aal/plugins/org.artop.ecuc.gautosar.accessors.check/src/org/artop/ecuc/gautosar/accessors/check/operations/AutosarCheckValidationOperation.java
@@ -15,21 +15,26 @@
 package org.artop.ecuc.gautosar.accessors.check.operations;
 
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 
 import org.artop.ecuc.gautosar.accessors.check.services.AutosarCheckProblemMarkerService;
+import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.common.util.Diagnostic;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.sphinx.emf.check.operations.BasicCheckValidationOperation;
 
 public class AutosarCheckValidationOperation extends BasicCheckValidationOperation {
 
-	public AutosarCheckValidationOperation(List<Object> modelObjects, Set<String> categories) {
-		super(modelObjects, categories);
+	public AutosarCheckValidationOperation(List<?> modelObjects, Map<Object, Object> options) {
+		super(modelObjects, options);
+	}
+
+	public AutosarCheckValidationOperation(String label, List<? extends Object> modelObjects, Map<Object, Object> options) {
+		super(label, modelObjects, options);
 	}
 
 	@Override
-	protected void updateProblemMarkers(EObject eObject, final Diagnostic diagnostic) {
+	protected void updateProblemMarkers(EObject eObject, Diagnostic diagnostic, IProgressMonitor monitor) {
 		AutosarCheckProblemMarkerService.INSTANCE.updateProblemMarkers(eObject, diagnostic);
 	}
 }
